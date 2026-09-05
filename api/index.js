aad94feb25ece8318b4bc54539e6e9477e13632b
Suninda Kumar Gayen <gayensuninda@gmail.com>
2026-09-05 23:05:41 +0530

    Fix Vercel API routing: point destination to /api and normalize req.url

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
 const app = require('../server.js');
 
-module.exports = app;
+module.exports = (req, res) => {
+  // Normalize incoming URL so Express routes matching /api/* are always matched
+  const targetUrl = req.originalUrl || req.url || '';
+  if (targetUrl.startsWith('/api')) {
+    req.url = targetUrl;
+  } else if (!req.url.startsWith('/api')) {
+    req.url = '/api' + (req.url.startsWith('/') ? '' : '/') + req.url;
+  }
+  return app(req, res);
+};
